ddf36242cf264a92c47150895c3aeea5649c9fa8
Fynnyx <westerathf@gmail.com>
2022-02-09 13:02:18 +0100

    add multiple roles on verify

--- security-bot/helpers/verify.js
+++ security-bot/helpers/verify.js
@@ -4,7 +4,9 @@ const data = require(`${process.cwd()}/properties.json`)
 
 exports.verifyMember = async (interaction) => {
     if (interaction.member.roles.cache.some(r => r.id === data.events.verify.memberrole)) return sendError(interaction, "You are already verified.", true, true);
-    interaction.member.roles.add(interaction.guild.roles.cache.get(data.events.verify.memberrole))
+    data.events.verify.addRoles.forEach(async (role) => {
+        interaction.member.roles.add(interaction.guild.roles.cache.get(role))
+    })
     sendSuccess(interaction, data.events.verify.verifiedmsg, true, true)
     await sleep(3)
     interaction.member.roles.remove(interaction.guild.roles.cache.get(data.events.verify.basicrole))
