90db33d6d6391b9d3987e75eed2a4bc6544191ca
Fynnyx <westerathf@gmail.com>
2022-02-16 08:49:48 +0100

    Issue #10 - Changed Api

--- kahlifar-bot/helpers/minecraft.js
+++ kahlifar-bot/helpers/minecraft.js
@@ -1,22 +1,17 @@
 const axios = require('axios');
-const data = require(`${process.cwd()}/properties.json`)
 
 exports.checkUsername = async (username) => {
     return axios(
         {
             method: "Get",
-            url: "https://minecraft-user-data.p.rapidapi.com/",
-            params: { name: username },
-            headers: {
-                'x-rapidapi-host': 'minecraft-user-data.p.rapidapi.com',
-                'x-rapidapi-key': 'bfbd923ff9msha833ad9b3459459p11ea25jsndbbc5a541ba9'
-            }
+            url: `https://api.ashcon.app/mojang/v2/user/${username}`,
         }
-    ).then(response => response.data)
-        .catch(error => {
-            if (error.response.status == 500) {
-                return 500
-            }
-            return undefined;
-        });
+    ).then(response => {
+        return response.data
+    }).catch(error => {
+        if (error.response.status == 500) {
+            return 500
+        }
+        return undefined;
+    });
 }
