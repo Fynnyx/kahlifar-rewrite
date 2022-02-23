fad03cab6e53f29bdd6edc7212985e3df44d8fd6
Fynnyx <westerath@gmail.com>
2022-02-23 16:39:12 +0100

    added start script

--- customstart.sh
+++ customstart.sh
@@ -1,2 +1,23 @@
 #!bin/bash
 
+STARTFOLDERS=("kahlifar-bot" "security-bot")
+WORKINGFOLDER=$( cd -- "$( dirname --  "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+printf "\nXxXxX[ Custom ]XxXxX\n"
+
+for BOTFOLDER in "${STARTFOLDERS[@]}"
+do 
+	if [ -d "$WORKINGFOLDER/$BOTFOLDER" ];
+	then
+		cd "$WORKINGFOLDER/$BOTFOLDER"
+		echo "${bold}$BOTFOLDER${normal}"
+		echo "[Info] - Stopping the session ..."
+		screen -X -S ${BOTFOLDER,,} kill
+		echo "[Info] - Starting bot ..."
+		screen -d -m -S ${BOTFOLDER,,} bash -c "node index.js"
+		echo "${bold}[Info] - Started $BOTFOLDER${normal}"
+    fi
+done
