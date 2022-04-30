1de1678d9bb334ba88607713919627bce2b871c3
Fynnyx <westerathf@gmail.com>
2022-04-30 17:58:33 +0200

    fixed discord command

--- KahlifarBot/commands/info/discord.js
+++ KahlifarBot/commands/info/discord.js
@@ -16,7 +16,6 @@ module.exports = {
 
     run: async (client, interaction, args) => {
         try {
-            TextDecoderStream()
             let linkMsg = ""
             data.commands.discord.links.map(value => {
                 linkMsg += `<${value}>\n`
