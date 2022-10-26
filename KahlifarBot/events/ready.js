d97ec80ddf4bc3550dd776a530eb6a92c44157d1
fwesterath <fwesterath@ethz.ch>
2022-10-26 11:08:55 +0200

    add video notification

--- KahlifarBot/events/ready.js
+++ KahlifarBot/events/ready.js
@@ -3,15 +3,19 @@ const { startStatus } = require("../helpers/status.js")
 const { startNotifications } = require("../helpers/streamNotification.js")
 const logger = require("../handlers/logger")
 const { startVideoNotifications } = require("../helpers/videoNotification.js")
+const axios = require("axios")
 
 
-client.on("ready", () => {
+client.on("ready", async () => {
     try {
-        console.info(`\x1b[33m${client.user.username}\x1b[34m, logged in\x1b[0m`)
         client.user.setActivity(`Starting...`)
+        console.info(`Starting status task`)
         startStatus()
+        console.info(`Starting stream notification task`)
         startNotifications()
-        // startVideoNotifications()
+        console.info("Starting video notification task");
+        startVideoNotifications()
+        console.info(`\x1b[33m${client.user.username}\x1b[34m, logged in\x1b[0m`)
     } catch (e) {
         logger.error(e)
     }
--- KahlifarBot/helpers/videoNotification.js
+++ KahlifarBot/helpers/videoNotification.js
@@ -8,89 +8,88 @@ const logger = require('../handlers/logger');
 
 exports.startVideoNotifications = async () => {
     const videoNotificationInterval = setInterval(async () => {
-        // try {
-            // const jwtkey = await getOAuthToken();
-            videoChannels.channels.forEach(async channel => {
-                // try {
-                    const index = videoChannels.channels.indexOf(channel);
-                    console.log("Checking for new videos from " + channel.id);
-                    console.log(await checkHasNewVideo(channel));
-                    if (await checkHasNewVideo(channel)) {
-                        const newVideos = await getNewVideos(channel);
-                        const channelData = await getChannelData(channel.id);
-                        for (video of newVideos) {
-                            const publishedDate = new Date(video.snippet.publishedAt);
-                            const publishedDateString = `${publishedDate.getHours()}:${publishedDate.getMinutes()}:${publishedDate.getSeconds()}\n${publishedDate.getDate()}.${publishedDate.getMonth() + 1}.${publishedDate.getFullYear()}`;
-                            const videoEmbed = new MessageEmbed()
-                                .setTitle(`📺 - ${video.snippet.channelTitle} hat ein neues Video hochgeladen`)
-                                .setURL(`https://www.youtube.com/watch?v=${video.id.videoId}`)
-                                .setDescription(`Schaue gerne bei - **${video.snippet.title}** - rein.\n\n${video.snippet.description}`)
-                                .setColor("#CF2626")
-                                .setThumbnail(video.snippet.thumbnails.high.url)
-                                .setImage(channelData.items[0].snippet.thumbnails.high.url)
-                                .setFields(
-                                    {
-                                        name: "Veröffentlicht am:",
-                                        value: `${publishedDateString}`,
-                                        inline: true
-                                    },
-                                    {
-                                        name: "-------------------------",
-                                        value: "\u200b",
-                                        inline: false
-                                    },
-                                    {
-                                        name: "Subscriber:",
-                                        value: `${channelData.items[0].statistics.subscriberCount}`,
-                                        inline: true
-                                    },
-                                    {
-                                        name: "Videos:",
-                                        value: `${channelData.items[0].statistics.videoCount}`,
-                                        inline: true
+        videoChannels.channels.forEach(async channel => {
+            try {
+                const channelItem = channel;
+                const index = videoChannels.channels.indexOf(channelItem);
+                if (await checkHasNewVideo(channelItem)) {
+                    const newVideos = await getNewVideos(channelItem);
+                    const channelData = await getChannelData(channelItem.id);
+                    for (video of newVideos) {
+                        const videoData = await getVideoData(video.id.videoId);
+                        const publishedDate = new Date(videoData.snippet.publishedAt);
+                        const publishedDateString = `${publishedDate.getHours()}:${publishedDate.getMinutes()}:${publishedDate.getSeconds()}\n${publishedDate.getDate()}.${publishedDate.getMonth() + 1}.${publishedDate.getFullYear()}`;
+                        const videoEmbed = new MessageEmbed()
+                            .setTitle(`📺 - ${videoData.snippet.channelTitle} hat ein neues Video hochgeladen`)
+                            .setURL(`https://www.youtube.com/watch?v=${videoData.id.videoId}`)
+                            .setDescription(`Schaue gerne bei - **${videoData.snippet.title}** - rein.\n\n${videoData.snippet.description}`)
+                            .setColor("#CF2626")
+                            .setThumbnail(channelData.snippet.thumbnails.high.url)
+                            .setImage(videoData.snippet.thumbnails.high.url)
+                            .setFields(
+                                {
+                                    name: "Veröffentlicht am:",
+                                    value: `${publishedDateString}`,
+                                    inline: true
+                                },
+                                {
+                                    name: "-------------------------",
+                                    value: "\u200b",
+                                    inline: false
+                                },
+                                {
+                                    name: "Subscriber:",
+                                    value: `${channelData.statistics.subscriberCount}`,
+                                    inline: true
+                                },
+                                {
+                                    name: "Videos:",
+                                    value: `${channelData.statistics.videoCount}`,
+                                    inline: true
 
-                                    }
-                                )
-                            }
-
+                                }
+                            )
                             const row = new MessageActionRow()
-                                .addComponents(
-                                    new MessageButton()
-                                        .setURL(`https://www.youtube.com/watch?v=${video.id.videoId}`)
-                                        .setLabel('Zum Video')
-                                        .setStyle('LINK')
-                                );
-                            
-                            const channel = await client.channels.fetch(data.helpers.videoNotification.notificationChannel);
-                            channel.send({ content: `<@&${data.helpers.streamerNotification.notificationRole}>`, embeds: [videoEmbed], components: [row] });
-                        }
-                // } catch (error) {
-                    // logger.error(error);
-                // }
-            }); 
-        // } catch (error) {
-            // logger.error(error);
-        // }           
+                            .addComponents(
+                                new MessageButton()
+                                    .setURL(`https://www.youtube.com/watch?v=${videoData.id}`)
+                                    .setLabel('Zum Video')
+                                    .setStyle('LINK')
+                            );
+    
+                        const channel = await client.channels.fetch(data.helpers.videoNotification.notificationChannel);
+                        channel.send({ content: `<@&${data.helpers.streamerNotification.notificationRole}>`, embeds: [videoEmbed], components: [row] });
+                        channelItem.lastVideoId = videoData.id;
+                        videoChannels.channels[index] = channelItem;
+                        writeFileSync('./videoChannels.json', JSON.stringify(videoChannels, null, 4));
+                    }
+                }
+            } catch (error) {
+                logger.error(error);
+            }
+        });
     }, data.helpers.videoNotification.intervalMinutes * 60 * 1000);
 }
 
 async function checkHasNewVideo(channel) {
-    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channel.id}&maxResults=1&order=date&type=video&key=${process.env.YT_API_KEY}`);
-    console.log(response.data);
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNis5z5HX_cVCiqOLVsZRZw&maxResults=10&order=date&type=video&key=AIzaSyAAD_ZTm_0FREQ31zTr2rBPIUUDNlQYOZw`);
     if (response.data.items.length === 0) {
         return false;
     } else if (response.data.items[0].id.videoId === channel.lastVideoId) {
         return false;
     }
-    console.log("lol2");
     return true;
 }
 
-async function getNewVideos(channelId) {
-    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&key=${process.env.YT_API_KEY}`);
+async function getNewVideos(channel) {
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channel.id}&key=${process.env.YT_API_KEY}`);
     let newVideos = [];
     for (item of response.data.items) {
         if (item.id.videoId === channel.lastVideoId) {
+            break;
+        }
+        const publishedAt = new Date(item.snippet.publishedAt);
+        if (publishedAt.getTime() < Date.now() - 1000 * 60 * 60 * 24 * 7) {
             break;
         }
         newVideos.push(item);
@@ -98,11 +97,12 @@ async function getNewVideos(channelId) {
     return newVideos;
 }
 
-async function getChannelData(channelId) {
-    const response = await axios.get(`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${process.env.YT_API_KEY}`);
-    return response.data;
+async function getVideoData(videoId) {
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&part=status,snippet,statistics,contentDetails,topicDetails&key=${process.env.YT_API_KEY}`);
+    return response.data.items[0];
 }
 
-async function getOAuthToken() {
-
+async function getChannelData(channelId) {
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/channels?part=brandingSettings,contentDetails,id,snippet,statistics&id=${channelId}&key=${process.env.YT_API_KEY}`);
+    return response.data.items[0];
 }
